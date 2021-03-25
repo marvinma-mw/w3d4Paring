e919a9bfb5e239bac649e79d0e819557e638670a
marvin <marvinma.mw@gmail.com>
2021-03-25 16:35:36 -0400

    question 03

--- starter/problems/03-batch-timeouts.js
+++ starter/problems/03-batch-timeouts.js
@@ -5,10 +5,10 @@ callback in the array with its corresponding delay. For example, the
 callback at index 0 should be set with the delay at index 0,
 the callback at index 1 should be set with the delay at index 1, and so on.
 The `batchTimeouts` function should return an array containing the Timeout
-objects for each timeout that was set. You may assume that both array arguments 
+objects for each timeout that was set. You may assume that both array arguments
 have the same length.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Example:
@@ -19,8 +19,8 @@ const shout = () => console.log('WHAT?');
 const tasks = [sayHello, sayGoodbye, shout];
 const delays = [500, 200, 900];
 
-const timeoutObjs = batchTimeouts(tasks, delays); 
-// should print: 
+const timeoutObjs = batchTimeouts(tasks, delays);
+// should print:
 //  'bye' after 200 ms
 //  'hi' after 500 ms
 //  'WHAT?' after 900 ms
@@ -28,7 +28,36 @@ const timeoutObjs = batchTimeouts(tasks, delays);
 console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
+function batchTimeouts(cb,delays){
+  for(let i=0;i<delays.length;i++){
+    let time=delays[i];
+    delays.push(time)
+    for(let j=0;j<cb.length;j++){
+      let obj=cb[j];
+      cb.push(obj)
+    if(cb=delays){
+      setTimeout(()=>{cb},delays)
+    }
+  }
+  }
+}
 
+
+
+
+const sayHello = () => console.log('hi');
+const sayGoodbye = () => console.log('bye');
+const shout = () => console.log('WHAT?');
+const tasks = [sayHello, sayGoodbye, shout];
+const delays = [500, 200, 900];
+
+const timeoutObjs = batchTimeouts(tasks, delays);
+// should print:
+//  'bye' after 200 ms
+//  'hi' after 500 ms
+//  'WHAT?' after 900 ms
+
+console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
