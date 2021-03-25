05bede3dfbc7f0dbcdcabd631834a61d906dbc7f
Andrew Jung <andrewattilajung@gmail.com>
2021-03-25 13:16:03 -0700

    02 complete

--- starter/problems/02-set-seconds-timeout-args.js
+++ starter/problems/02-set-seconds-timeout-args.js
@@ -20,9 +20,23 @@ setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
 setSecondsTimeoutArgs(function(arg1, arg2) {
     console.log(arg1 + '-' + arg2);
 }, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
+
 ***********************************************************************/
 
+function setSecondsTimeoutArgs(cb, time, ...values) {
+  setTimeout(() => {cb(...values)}, time * 1000);
+}
 
+
+
+function printSum(num1, num2, num3) {
+  console.log(num1 + num2 + num3);
+}
+setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
+
+setSecondsTimeoutArgs(function(arg1, arg2) {
+  console.log(arg1 + '-' + arg2);
+}, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
